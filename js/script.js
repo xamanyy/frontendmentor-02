14616e7ca2c296ed25ddabdf9695ebfbc474779e
Xamanyy <sing40261@gmail.com>
2024-01-08 12:53:48 +0530

    feature-added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-console.log("hellport")
 
 var element0 = document.querySelector('.para1');
 var element1 = document.querySelector('.para2');
@@ -16,23 +15,70 @@ if (element0 && element1 && element2 && element3) {
     console.error("Element not found");
 }
 
+function toggleParagraph(pNumber){
+        
+        var imgElement =  document.querySelector(`.plus${pNumber}`);
 
-const btn1 = document.querySelector('.btn1');
-btn1.addEventListener('click',function(){
-    element0.style.display = 'block';
-})
+        //Hiding all paragraphs.
+        for(let i = 1;i <=4;i++){
+            var img =  document.querySelector(`.plus${i}`);
+            document.querySelector(`.para${i}`).style.display = 'none';
+            img.src ="./assets/images/icon-plus.svg";
+        }
 
-const btn2 = document.querySelector('.btn2');
-btn2.addEventListener('click',function(){
-    element1.style.display = 'block';
-})
+        //show the selected paragraphs.
+        
+        document.querySelector(`.para${pNumber}`).style.display = 'block';
+        imgElement.src ="./assets/images/icon-minus.svg";
+}
 
-const btn3 = document.querySelector('.btn3');
-btn3.addEventListener('click',function(){
-    element2.style.display = 'block';
-})
 
-const btn4 = document.querySelector('.btn4');
-btn4.addEventListener('click',function(){
-    element3.style.display = 'block';
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btn1 = document.querySelector('.btn1');
+// btn1.addEventListener('click',function(){
+//     element0.style.display = 'block';
+// })
+
+// const btn2 = document.querySelector('.btn2');
+// btn2.addEventListener('click',function(){
+//     element1.style.display = 'block';
+// })
+
+// const btn3 = document.querySelector('.btn3');
+// btn3.addEventListener('click',function(){
+//     element2.style.display = 'block';
+// })
+
+// const btn4 = document.querySelector('.btn4');
+// btn4.addEventListener('click',function(){
+//     element3.style.display = 'block';
+// })
